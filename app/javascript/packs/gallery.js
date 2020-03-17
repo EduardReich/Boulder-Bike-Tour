f889d14e7ee182d8b7f5fe0cf7a888f6e2617447
EduardReich <eduard_reich@hotmail.com>
2020-03-17 02:20:06 +0100

    flickr api

---
+++ app/javascript/packs/gallery.js
@@ -0,0 +1,20 @@
+var tester = document.getElementById("test");
+let apiRequest = new XMLHttpRequest();
+const apiKey = "d9f919603b3f6e079ee3a340db3dbb15";
+
+window.addEventListener("load", () => {
+  apiRequest.open("GET", "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key="+ apiKey +"&tags=bikerace%2C+boulderbiketour&per_page=500&format=json&nojsoncallback=1");
+  apiRequest.send();
+});
+
+apiRequest.onreadystatechange = () => {
+  var response = JSON.parse(apiRequest.response);
+  var photos = response.photos.photo;
+  console.log(photos);
+  
+  for (let i of photos) {
+    var pictures = "https://farm" + i.farm +".staticflickr.com/" + i.server + "/" + i.id + "_" + i.secret + ".jpg";
+    tester.innerHTML += "<img src='" + pictures + "' ><br>"; 
+  }  
+  
+}
