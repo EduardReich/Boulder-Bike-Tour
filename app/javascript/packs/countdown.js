a13500fa03dcf09fe14c0c1b643a67eb70e497a4
EduardReich <eduard_reich@hotmail.com>
2020-04-03 07:54:11 +0200

    riders controller

---
+++ app/javascript/packs/countdown.js
@@ -0,0 +1,21 @@
+let countDownDate = new Date("Apr 1, 2021 15:37:25").getTime();
+
+let x = setInterval(function () {
+
+  let now = new Date().getTime();
+
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("counter").innerHTML = days + " Days " + hours + " Hours "
+    + minutes + " Minutes " + seconds + " Seconds left until Bolder Bike Tour 2021";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("counter").innerHTML = "EXPIRED";
+  }
+}, 1000);
--- app/javascript/packs/gallery.js
+++ app/javascript/packs/gallery.js
@@ -1,9 +1,8 @@
 const apiKey = "d9f919603b3f6e079ee3a340db3dbb15";
 const loadMoreBtn = document.getElementById("loadMoreBtn");
-let photosContainer = document.getElementById("photosContainer");
-let page = 2;
+const photosContainer = document.getElementById("photosContainer");
 
-let state = {
+const state = {
   page : 1
 }
 
@@ -11,9 +10,9 @@ function getPhotos(page) {
   fetch('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key='+ apiKey +'&tags=bikerace%2C+boulderbiketour&per_page=40&page='+page+'&format=json&nojsoncallback=1')
 	  .then(response => response.json())
 	  .then((data) => {
-	    let result = data.photos.photo;
-      let endResult = [];
-      for(let i of result) {
+	    const result = data.photos.photo;
+      const endResult = [];
+      for(const i of result) {
         let pictures = "https://farm" + i.farm +".staticflickr.com/" + i.server + "/" + i.id + "_" + i.secret + ".jpg";
         endResult.push("<img src='" + pictures + "' >");
       }     
